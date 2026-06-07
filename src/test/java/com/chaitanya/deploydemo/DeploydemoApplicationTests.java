20fcbf3147bcfe9eff34e90c3cb062cc4dc30601
chaitanya <chaitanyavankhande4@gmail.com>
2026-06-07 15:03:00 +0530

    basic setup

---
+++ src/test/java/com/chaitanya/deploydemo/DeploydemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chaitanya.deploydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeploydemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
