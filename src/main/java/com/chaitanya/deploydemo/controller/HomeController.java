d726041d963406e33bee080a8c14d53fccd997be
chaitanya <chaitanyavankhande4@gmail.com>
2026-06-07 15:14:55 +0530

    home and welcome apis

---
+++ src/main/java/com/chaitanya/deploydemo/controller/HomeController.java
@@ -0,0 +1,21 @@
+package com.chaitanya.deploydemo.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/home")
+public class HomeController {
+
+    @GetMapping
+    public ResponseEntity<String> home() {
+        return ResponseEntity.ok("Home Page");
+    }
+
+    @GetMapping("/welcome")
+    public ResponseEntity<String> welcome() {
+        return ResponseEntity.ok("Welcome");
+    }
+}
